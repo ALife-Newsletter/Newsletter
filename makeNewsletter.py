e6c3c998ad560f694ff387486d40f7e6291ba031
Claus Aranha <caranha@gmail.com>
2022-03-13 20:00:40 +0900

    changed makeNewsletter script to write html newsletter to a specific output directory

--- makeNewsletter.py
+++ makeNewsletter.py
@@ -1,30 +1,33 @@
 import markdown
-import os
+import os, shutil
+import re
 import argparse
 
 if __name__ == "__main__":
+    # Part I -- configure and validate the script from parameters
     parser = argparse.ArgumentParser(description="Create a newsletter html")
     parser.add_argument("directory", metavar="directory", type=str,
                         help="directory with the newsletter contents")
-    directory = (parser.parse_args()).directory
+    target = (parser.parse_args()).directory
 
-    if not os.path.isdir(directory):
-        raise ValueError("{} is not a directory".format(directory))
+    if not os.path.isdir(target):
+        raise ValueError("Can't find directory {}".format(target))
 
+
+    # Part II -- generate the newsletter from the md files
     text = []
 
-    files = os.listdir(directory)
+    files = os.listdir(target)
     files.sort()
 
     for filename in files:
         if filename.endswith(".md"):
             if filename.startswith("00"):
-                with open(os.path.join(directory, filename), "r") as f:
+                with open(os.path.join(target, filename), "r") as f:
                     opening = f.read()
             else:
-                with open(os.path.join(directory, filename), "r") as f:
+                with open(os.path.join(target, filename), "r") as f:
                     text.append(f.read())
-
     bodytext = "\n".join(text)
 
     md = markdown.Markdown(extensions=['toc'])
@@ -33,6 +36,7 @@ if __name__ == "__main__":
     htmlbody = md.convert(bodytext)
     htmltoc = md.toc
 
+    # Part 3 save the result in the publish directory
     with open(os.path.join("static", "header.html")) as f:
         htmlhead = f.read()
 
@@ -47,8 +51,21 @@ if __name__ == "__main__":
     htmlfooter
     ]
 
-    with open(os.path.join(directory, "newsletter.html"), "w") as o:
-        o.write("\n".join(html))
+
+    htmltext = "\n".join(html).replace('"images/',
+                                       '"images_{}/'.format(target))
+    with open(os.path.join("publish", target+".html"), "w") as o:
+        o.write(htmltext)
+
+    # copying images
+    source_image_dir = os.path.join(target, "images")
+    dest_image_dir = os.path.join("publish", "images_"+target)
+
+    if not os.path.isdir(dest_image_dir):
+        os.mkdir(dest_image_dir)
+    for file in os.listdir(source_image_dir):
+        shutil.copyfile(os.path.join(source_image_dir, file),
+                        os.path.join(dest_image_dir, file))
 
     print("done.")
 
