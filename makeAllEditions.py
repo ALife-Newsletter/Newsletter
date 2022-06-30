976fd7fbe11853ecbe6aeb29a92325647c5de9ff
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2022-06-30 17:27:13 +0900

    Replaced git commands to raw shell commands to make it more customizable

--- makeAllEditions.py
+++ makeAllEditions.py
@@ -4,7 +4,7 @@ def collect_content_directory_names():
     return [name for name in os.listdir(".") if os.path.isdir(name) and name.startswith(content_directory_prefix)]
 
 if __name__ == "__main__":
-    raise ValueError("test")
+    #raise ValueError("test")
     from makeNewsletter import make_newsletter
     edition_names = collect_content_directory_names()
     for edition_name in edition_names:
