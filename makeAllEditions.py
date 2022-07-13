d2a10aef7a8d9cc2ef181158436c65186929f9c3
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2022-07-13 16:40:22 +0900

    [Do Not Merge] Add an exception on the build phase

--- makeAllEditions.py
+++ makeAllEditions.py
@@ -1,6 +1,7 @@
 from utilities import collect_content_directory_names
 
 if __name__ == "__main__":
+    raise ValueError("expected fail")
     from makeNewsletter import make_newsletter
     edition_names = collect_content_directory_names()
     for edition_name in edition_names:
