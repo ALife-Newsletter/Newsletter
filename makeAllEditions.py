9964eeeeb5412890a4387538d4fa3ee7c1468d9d
Claus Aranha <caranha@gmail.com>
2026-06-14 11:48:57 +0900

    Action Test

--- makeAllEditions.py
+++ makeAllEditions.py
@@ -1,8 +1,11 @@
 from utilities import collect_content_directory_names
+from makeNewsletter import make_newsletter
+from makeRSS import makeRSS
+
+# This is the main script that generates the ALIFE Newsletter
+
 
 if __name__ == "__main__":
-    from makeNewsletter import make_newsletter
-    from makeRSS import makeRSS
     edition_names = collect_content_directory_names()
     for edition_name in edition_names:
         make_newsletter(edition_name)
